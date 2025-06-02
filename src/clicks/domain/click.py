b1664e03839a38024d057de264bbcefd29bb2117
IldarkoS <ildar.org.ru@gmail.com>
2025-06-02 17:33:58 +0300

    create clicks table and migration

---
+++ src/clicks/domain/click.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.core.models.base import Base
+
+if TYPE_CHECKING:
+    from src.urls.domain.url import URL
+
+
+class Click(Base):
+    url_id: Mapped[int] = mapped_column(
+        ForeignKey("urls.id", ondelete="CASCADE"),
+    )
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+
+    url: Mapped["URL"] = relationship(back_populates="clicks")
--- src/core/models/metadata.py
+++ src/core/models/metadata.py
@@ -2,8 +2,10 @@ __all__ = (
     "Base",
     "URL",
     "User",
+    "Click"
 )
 
+from src.clicks.domain.click import Click
 from src.core.models.base import Base
 from src.urls.domain.url import URL
 from src.users.domain.user import User
